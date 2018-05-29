aafe59c04cf5a9aa54ff724e0edbfbef4ead26ad
Bryan VALCASARA <brvalcas@e2r2p4.42.fr>
2018-05-29 02:39:37 +0200

    modif du jour

--- fillit.h
+++ fillit.h
@@ -13,12 +13,12 @@
 #ifndef FILLIT_H
 # define FILLIT_H
 
-# include "libft.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
+# include <unistd.h>
 # define BUFF_SIZE 21
 
 typedef struct		s_tetri
@@ -48,6 +48,13 @@ int					min_square_finder(int len);
 char				*create_square(int size);
 char				**tab_lib_creator(char *id_piece, t_tetri *list);
 int					get_square_size(char *sq);
-char				*tetrisolver(char *sq, char c, int start, char **tab_pieces);
+char				*tetrisolver(char *sq, char c, int start,
+					char **tab_pieces);
 int					get_start(char *sq, char c);
+void				ft_putchar(char c);
+void				ft_putstr(char const *s);
+int					ft_strcmp(const char *s1, const char *s2);
+char				*ft_strdup(const char *s1);
+size_t				ft_strlen(const char *s);
+
 #endif
--- ft_strdup.c
+++ ft_strdup.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "fillit.h"
 
 char	*ft_strdup(const char *s1)
 {
